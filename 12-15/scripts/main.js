ad170ab9cad41948bbba818f103f8ed4902e1966
tikokk <405269689@qq.com>
2020-10-07 16:24:11 +0800

    第十六天啦

---
+++ 12-15/scripts/main.js
@@ -0,0 +1,31 @@
+function show(){
+  var s1  =document.getElementById("c13");
+  var s2  =document.getElementById("c12");
+  var s3  =document.getElementById("A1");
+  var s4  =document.getElementById("A2");
+  var s5  =document.getElementById("q1");
+  var s6  =document.getElementById("q2");
+  
+  s1.style.display="block"
+  s2.style.display="none"
+  s3.style.backgroundColor="white"
+  s3.style.color="black"
+  s4.style.backgroundColor="transparent"
+  s4.style.color="white"
+}
+function show2(){
+  var s1  =document.getElementById("c13");
+  var s2  =document.getElementById("c12");
+  var s3  =document.getElementById("A1");
+  var s4  =document.getElementById("A2");
+  var s5  =document.getElementById("q1");
+  var s6  =document.getElementById("q2");
+  
+  s1.style.display="none"
+  s2.style.display="block"
+  s4.style.backgroundColor="white"
+  s4.style.color="black"
+  s3.style.backgroundColor="transparent"
+  s3.style.color="white"
+
+}
---
+++ 16 days/scripts/main.js
@@ -0,0 +1,7 @@
+document.getElementById("qsh").onclick = function(){
+  var qqq= document.getElementById("yc")
+ if(qqq.style.display==='none'){
+   qqq.style.display='block';
+ }
+  else{qqq.style.display='none'}
+}
